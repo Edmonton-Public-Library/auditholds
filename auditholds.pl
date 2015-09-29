1f59d18677d4176d80e18498cf6cd9ab6c6be70f
Andrew Nisbet <anisbet@epl.ca>
2015-09-29 16:34:28 -0600

    3 report types now, the last one is supposed to report all uniq callnums on a title but misses one. Investigate.

--- auditholds.pl
+++ auditholds.pl
@@ -48,7 +48,7 @@ my $TIME       = `date +%H%M%S`;
 chomp $TIME;
 my $DATE       = `date +%m/%d/%Y`;
 chomp $DATE;
-my $VERSION    = qq{0.3};
+my $VERSION    = qq{0.4};
 
 
 
@@ -65,6 +65,11 @@ This script reports problems with holds, specifically orphaned holds.
 An orphaned hold is one that exists on a call number that has no visible 
 item copies.
 
+ -e: Print exhaustive report, that is, all multi-callnum titles with holds
+     and multi-callnum titles with holds with callnums with no visible copies.
+ -t: Test script. Doesn't remove temporary files so they can be reviewed.
+ -v: Ignore obvious volume situations, that is, drop titles that contain
+     callnums related to volumes.
  -x: This (help) message.
 
 example: $0 -x
@@ -97,14 +102,26 @@ sub create_tmp_file( $$ )
 # return: 
 sub init
 {
-    my $opt_string = 'x';
+    my $opt_string = 'etvx';
     getopts( "$opt_string", \%opt ) or usage();
     usage() if ( $opt{'x'} );
+    printf STDERR "Test mode active. Temp files will not be deleted. Please clean up '%s' when done.\n", $TEMP_DIR if ( $opt{'t'} );
+}
+
+# Tests if a line ('flex_key|callnum') looks like a multi-volume title.
+# param:  line as above.
+# return: 1 if the title is a multi-volume hold and 0 if title is not or the results are unclear.
+sub is_multi_volume_title( $ )
+{
+	my $line = shift;
+	return 1 if ( $line =~ m/\s+(19|20)\d{2}$/ ); # lines for multivolume sets typically end with a year.
+	return 1 if ( $line =~ m/\s+(v|V|bk|BK)\.\d{1,}$/ ); # lines for multivolume sets typically end with v.##.
+	return 0;
 }
 
 # Formats and prints a report of findings.
 # param:  String header message.
-# param:  result string from API calls: 'TCN| call number | visible copies'.
+# param:  result string from API calls: '999407|55|DVD NOT AVAILABLE|0|'.
 # return: <none>
 sub print_report( $$ )
 {
@@ -121,6 +138,7 @@ sub print_report( $$ )
 	while ( @lines )
 	{
 		my $line = shift @lines;
+		next if ( is_multi_volume_title( $line ) and $opt{'v'} );
 		my ( $tcn, $callnum ) = split '\|', $line;
 		format STDOUT =
 @>>>>>>>>>>> @<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
@@ -132,32 +150,71 @@ $tcn, $callnum
 
 init();
 
-
-# Output all cat keys for titles with holds that have 2 or more distinct callnums. 
-my $results = `selhold -j"ACTIVE" -a"N" -t'T' -oN 2>/dev/null | sort | uniq | selcallnum -iN -oCD 2>/dev/null | pipe.pl -dc1 -A | pipe.pl -W"\\s+" -C'c0:ge2' -o'c1' -s'c1'`;
-my $master_list = create_tmp_file( "cat_keys", $results );
-if ( -s $master_list )
+# This is a description of the following selection broken down by pipe boundary.
+# From the HOLD table, select all active non-available holds and output the cat key.
+# sort the keys.
+# unique the keys.
+# select from the CALLNUM table all the entries based on supplied cat keys and output the callnum key, callnum, and visible items.
+my $results = `selhold -j"ACTIVE" -a'N' -oC 2>/dev/null | sort | uniq | selcallnum -iC -oNDz 2>/dev/null`;
+my $master_list = create_tmp_file( "audithold_callnum_keys", $results );
+# So now we have all the callnum keys, callnums, and visible items.
+# This is a description of the following selection broken down by pipe boundary.
+# cat the list of call nums which looks like '997099|38|Teen fiction - Series H PBK|0|'
+# Take input into pipe.pl and dedup on the callnum and cat key in that order. This gives a list of cat keys and unique callnumbers.
+# Use the results and sort by cat key and callnum, then dedup again this time on cat key and output a count of dedups delimited by a '|'.
+# Take that output and only display the lines that have a count greater than one, that is, output the cat keys that have more than
+# 1 different callnumbers.
+$results = `cat "$master_list" | pipe.pl -d'c2,c0' | pipe.pl -s'c1,c3' -d'c0' -A -P | pipe.pl -C'c0:gt1' -o'c1'`;
+my $cat_key_list = create_tmp_file( "audithold_cat_keys", $results );
+# Tidy up the master list, if there is one.
+unlink $master_list if ( -s $master_list and ! $opt{'t'} );
+if ( -s $cat_key_list )
 {
-	$results = `cat "$master_list" | selcatalog -iC -oCF 2>/dev/null | selcallnum -iC -oNDzS 2>/dev/null | pipe.pl -dc1 -o'c4,c2,c3' -C'c3:eq0' | pipe.pl -sc0 -dc1 -tc0`;
-	my $non_visible_callnums = create_tmp_file( "non_visible_callnums", $results );
-	if ( -s $non_visible_callnums )
+	# This is a description of the following selection broken down by pipe boundary.
+	# cat the list of cat keys selected above.
+	# select from CALLNUM all cat keys, output call number key, call number, and count of visible items under the callnum.
+	# 999466|85|TEEN LOR|1|
+	# 999466|84|Teen fiction - Series I PBK|1|
+	# 999524|145|TEEN SAE|1|
+	# 999524|53|Teen fiction S|vPBK|0|
+	# 999524|147|Teen fiction S PBK|1|
+	# 999407|59|DVD 617.58204 BOW|1|
+	# 999407|55|DVD NOT AVAILABLE|0|
+	$results = `cat "$cat_key_list" | selcallnum -iC -oNDz 2>/dev/null | pipe.pl  -d'c0,c2' | pipe.pl -s'c0'`;
+	my $candidate_callnums = create_tmp_file( "audithold_candidate_callnums", $results );
+	if ( -s $candidate_callnums )
 	{
-		my $count = `cat "$non_visible_callnums" | wc -l`;
+		my $count = `cat "$cat_key_list" | wc -l | pipe.pl -t'c0'`;
 		chomp $count;
-		my $header = sprintf "%d call numbers with hold issues.", $count;
-		$results = `cat "$non_visible_callnums"`;
+		my $header = sprintf "%d multi-callnum titles with holds.", $count;
+		# The first report will output all titles with holds that have multiple callnums.
+		$results = `cat "$candidate_callnums" | selcatalog -iC -oFS 2>/dev/null | pipe.pl -t'c0' -o'c0,c2' -s'c0'`;
+		print_report( $header, $results ) if ( $opt{'e'} );
+		# else just print the report of those titles with call nums with no visible items.
+		my $zero_count = `cat "$candidate_callnums" | pipe.pl -C'c3:eq0' | wc -l | pipe.pl -t'c0'`;
+		chomp $zero_count;
+		$header = sprintf "%d multi-callnum titles with hold, %d with 0 visible copies.", $count, $zero_count;
+		# This is a description of the following selection broken down by pipe boundary.
+		# cat the list of cat keys selected above.
+		# select from CATALOG from input of cat keys, output the TCN and `o785243323        |55|DVD NOT AVAILABLE|0|`.
+		# take the input trim the flex key, output flex key and callnum if the number of visible items under callnum is 0.
+		$results = `cat "$candidate_callnums" | selcatalog -iC -oFS 2>/dev/null | pipe.pl -t'c0' -o'c0,c2' -C'c3:eq0' -s'c0'`;
 		print_report( $header, $results );
-		unlink $non_visible_callnums;
+		# Now I want to see a list of all the titles, with the different call numbers under it.
+		$results = `cat "$cat_key_list" | selcatalog -iC -oCF 2>/dev/null | selcallnum -iC -oSD 2>/dev/null | pipe.pl -t'c0' -d'c1,c0' | pipe.pl -s'c0'`;
+		print_report( "", $results );
+		# Tidy up the master list, if there is one.
+		unlink $cat_key_list if ( -s $cat_key_list and ! $opt{'t'} );
+		unlink $candidate_callnums if ( -s $candidate_callnums and ! $opt{'t'} );
 	}
 	else
 	{
 		printf STDERR "No call numbers with 0 visible items found.\n";
 	}
-	unlink $master_list;
 }
 else
 {
-	printf STDERR "*** error creating temp file '$master_list'.\n";
+	printf STDERR "No problems detected.\n";
 	exit 0;
 }
 # EOF
